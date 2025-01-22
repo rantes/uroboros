b2a1ac4eb78434e9f6c2a4e063ee9a7c4137c020
Javier Serrano <rantes.javier@gmail.com>
2025-01-21 19:02:35 -0500

    Udate index file for new DumboPHP

--- app/webroot/index.php
+++ app/webroot/index.php
@@ -1,26 +1,28 @@
 <?php
-session_status() === PHP_SESSION_ACTIVE or session_start();
 $dir = realpath('./../');
 defined('INST_PATH') || define('INST_PATH', dirname($dir).'/');
 set_include_path(
     '/etc/dumbophp'.PATH_SEPARATOR.
     '/etc/dumbophp/bin'.PATH_SEPARATOR.
+    '/etc/dumbophp/lib'.PATH_SEPARATOR.
     INST_PATH.'vendor'.PATH_SEPARATOR.
     INST_PATH.'vendor/rantes/dumbophp'.PATH_SEPARATOR.
     INST_PATH.'vendor/rantes/dumbophp/bin'.PATH_SEPARATOR.
+    INST_PATH.'vendor/rantes/dumbophp/lib'.PATH_SEPARATOR.
     INST_PATH.PATH_SEPARATOR.
     get_include_path().PATH_SEPARATOR.
     PEAR_EXTENSION_DIR.PATH_SEPARATOR.
     '/windows/dumbophp'.PATH_SEPARATOR.
     '/windows/dumbophp/bin'.PATH_SEPARATOR.
+    '/windows/dumbophp/lib'.PATH_SEPARATOR.
     '/windows/system32/dumbophp'.PATH_SEPARATOR.
     '/windows/system32/dumbophp/bin'.PATH_SEPARATOR.
+    '/windows/system32/dumbophp/lib'.PATH_SEPARATOR.
     INST_PATH.'DumboPHP'
 );
 
 require_once 'dumbophp.php';
 require_once INST_PATH.'config/host.php';
 if (file_exists(INST_PATH.'vendor/autoload.php')) require_once INST_PATH.'vendor/autoload.php';
-
 $index = new index();
-?>
+$index->page->display();
