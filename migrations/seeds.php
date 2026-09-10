1ee1f9a1d660065b3692a9d0668b413f30eab6ec
Javier Serrano <rantes.javier@gmail.com>
2026-09-10 07:36:14 -0500

    Eliminación de valores sensibles en las semillas

--- migrations/seeds.php
+++ migrations/seeds.php
@@ -5,28 +5,8 @@ use DumboPHP\Controller;
 
 class Seeds extends Controller {
 
-    private function _sowAdmin() {
-        $user = $this->AppUser->Find(['fields'=>'id', 'conditions'=>"`email`='admin@admin.com'"]);
-        if (0 === $user->counter()):
-            $user = $this->AppUser->Niu([
-                'email' => 'admin@admin.com',
-                'firstname' => 'Admin',
-                'lastname' => 'Admin',
-                'document' => '88267145',
-                'document_kind_id' => 1,
-                'phone' => 123456,
-                'password' => '1234567890',
-                'status' => 1,
-                'level' => 32,
-            ]);
-            $user->Save() or die($user->_error);
-        endif;
-    }
-
     public function sow(?array $actions = []): void {
-        $defaults = [
-            '_sowAdmin'
-        ];
+        $defaults = [];
         $actions = empty($actions) ? $defaults : $actions;
         foreach ($actions as $action):
             $this->{$action}();
